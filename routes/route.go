63dbd9fce1a5c616addb683f22c1500886eb9a81
hzmarrou <zmarrouhoussam@email.com>
2026-04-10 08:19:24 +0100

    add routes for comments and reactions

--- routes/route.go
+++ routes/route.go
@@ -1,12 +1,14 @@
 package routes
 
 import (
-	"net/http"
-
 	"forum/handlers"
+	"forum/middleware"
+	"net/http"
 )
 
 func Route() {
 	http.HandleFunc("/register", handlers.RegisterHandler)
 	http.HandleFunc("/do-register", handlers.DoRegisterHandler)
+	http.Handle("/comment", middleware.AuthMiddleware(http.HandlerFunc(handlers.AddCommentHandler)))
+	http.Handle("/react", middleware.AuthMiddleware(http.HandlerFunc(handlers.ReactionHandler)))
 }
