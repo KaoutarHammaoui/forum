6359fb8eb726324f1187dd050a6dad4209e13a58
kuuhaku <kuuhaku@kuu.com>
2026-04-18 15:14:34 +0100

    fix the code and intit the project

--- internal/cmd/server.go
+++ internal/cmd/server.go
@@ -2,12 +2,14 @@ package cmd
 
 import (
 	"fmt"
+	"forum/internal/bootstrap"
 	"log"
 	"net/http"
 )
 
 
 func Serve(){
+	bootstrap.Init()
 	log.Println("http://localhost:8080")
 	err := http.ListenAndServe(":8080", nil)
 	if err != nil {
--- internal/handler/homeHandler.go
+++ internal/handler/homeHandler.go
@@ -1,7 +1,33 @@
 package handler
 
-import "net/http"
+import (
+	"forum/internal/config"
+	"forum/internal/middleware"
+	models "forum/internal/model"
 
-func Index(w http.ResponseWriter, r *http.Request){
-	
-}	
+	"net/http"
+)
+
+type Data struct {
+	Title      string
+	IsLoggedIn bool
+	Categories []models.Category
+}
+
+func Index(w http.ResponseWriter, r *http.Request) {
+	if r.Method != http.MethodGet && r.Method != http.MethodPost {
+		HandleError(w, "Method not allowed", http.StatusMethodNotAllowed)
+		return
+	}
+
+	_, loggedIn := middleware.IsAuthenticated(r)
+
+	data := Data{
+		Title:      "Home",
+		IsLoggedIn: loggedIn,
+		Categories: []models.Category{}, // replace with real data later
+	}
+
+	// Example response (you'll likely use templates instead)
+	config.RenderTemplate(w, "index.html", data)
+}
